6c36b118529bd30379b31c9f49d83ea29de99343
Fatema094 <fatematussumaiya16@gmail.com>
2024-09-28 13:13:08 +0600

    chatapp

---
+++ app/src/main/java/com/example/project1/listeners/ConversionListener.java
@@ -0,0 +1,7 @@
+package com.example.project1.listeners;
+
+import com.example.project1.models.User;
+
+public interface ConversionListener {
+    void onConversionClicked(User user);
+}
---
+++ app/src/main/java/com/example/project1/listeners/UserListener.java
@@ -0,0 +1,9 @@
+package com.example.project1.listeners;
+
+import com.example.project1.models.User;
+
+public interface UserListener {
+
+    void onUserClicked(User user);
+
+}
